de42805f040f8aa62b1a5606cdf8503ab3de77e5
Kuldipak Kadam <kuldipakkadam2004@gmail.com>
2025-08-29 10:28:06 +0530

    small change

--- utils/token-utils.js
+++ utils/token-utils.js
@@ -3,16 +3,16 @@ const crypto = require('crypto');
 const tokenStore = {}; //Simplified token store
 
 const generateToken = () => {
-    return crypto.randomBytes(16).toString('hex'); // 32 char hex string
+    return crypto.randomBytes(16).toString('hex'); //32 char hex string
 };
 
-exports. generateAndStoreToken = (userId) => {
+exports.generateAndStoreToken= (userId) => {
     const token = generateToken();
     tokenStore[userId] = token;
     return token;
 };
 
-exports.validateUserToken = (userId, incomingToken) => {
+exports.validateUserToken= (userId, incomingToken) => {
     const savedToken = tokenStore[userId];
     if (!savedToken) return false;
 
